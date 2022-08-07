eca2f23d98a171bc16cfdf5632679fd1e11b255f
peihao.chen <jobs894117@gmail.com>
2022-08-07 14:28:48 +0900

    first commit

---
+++ example1.py
@@ -0,0 +1,36 @@
+# input, output:
+# if else
+
+# int("string") -> 
+# var = int(var)
+
+# var = input("apple")
+# print(var)
+
+
+# input -> string
+# string -> int -> sub, add
+
+var1 = input("Enter a original number: ")
+# var2 = input("b")
+var1 = int(var1)
+# var2 = int(var2)
+# if var1 >= var2:
+# 	print("OK")
+# else:
+# 	print("NG")
+
+# True, False
+
+while var1 <= 1000:
+	print("var1")
+	break
+
+
+
+
+
+
+
+
+
---
+++ main.py
@@ -0,0 +1,32 @@
+# Tell pyhthon not to read
+
+# lesson 1: in & out
+# print("Hello world")
+
+# # lesson 2: if & else
+# var = input("Input: ")
+
+# if var >= "a":
+# 	print("bingo")
+# else:
+# 	print("False")
+
+# ==, >=, <= 
+# lesson 3: data type
+string = "Hello world"
+char = "a"
+integer = -1
+float_ = 3.14159
+# boolean = True, False
+
+# lesson 4: for & while
+
+# for i in range(1000):
+# 	print(i)
+# 	print("apple")
+
+i = 0
+while i < 1000:
+	print(i)
+	# i *= 2
+	i = i + 1
